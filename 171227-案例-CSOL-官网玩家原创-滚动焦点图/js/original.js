c149163465a49488bb512e4e3f727aee190742a1
Macintosh <39333377+Lifeispractice@users.noreply.github.com>
2019-06-17 17:03:24 +0800

    Add files via upload

---
+++ 171227-案例-CSOL-官网玩家原创-滚动焦点图/js/original.js
@@ -0,0 +1,61 @@
+document.writeln(
+'  <div class="original animate_four animate_hidden" >'+
+'	<div class="tit">'+
+'		<p>玩家原创</p>'+
+'		<a href="https://csol.tiancity.com/homepage/event/2017/07/cs170720map/" target="_blank">更多&gt;&gt;</a>'+
+'	</div>'+
+'	<div class="original-box">'+
+'		<div class="marquee">'+
+'		<ul>'+
+'			<li><a href="javascript:;" ><img src="https://img1.tiancitycdn.com/project1/csol/event/2016/10/cs161025ver/images/3.jpg" /></a></li>'+
+'			<li><a href="javascript:;" ><img src="https://img1.tiancitycdn.com/project1/csol/event/2016/10/cs161025ver/images/4.jpg" /></a></li>'+
+'			<li><a href="javascript:;" ><img src="https://img1.tiancitycdn.com/project1/csol/event/2016/10/cs161025ver/images/1.jpg" /></a></li>'+
+'			<li><a href="javascript:;" ><img src="https://img1.tiancitycdn.com/project1/csol/event/2016/10/cs161025ver/images/2.jpg" /></a></li>'+
+
+'			<li><a href="javascript:;" ><img src="https://img1.tiancitycdn.com/project1/csol/event/2018/04/cs180411act/images/a.jpg" /></a></li>'+
+'			<li><a href="javascript:;" ><img src="https://img1.tiancitycdn.com/project1/csol/event/2018/04/cs180411act/images/b.jpg" /></a></li>'+
+'			<li><a href="javascript:;" ><img src="https://img1.tiancitycdn.com/project1/csol/event/2018/04/cs180411act/images/180416hd.jpg" /></a></li>'+
+'			<li><a href="javascript:;" ><img src="https://img1.tiancitycdn.com/project1/csol/event/2017/07/cs-map/170929/180228898.jpg" /></a></li>'+
+'			<li><a href="javascript:;" ><img src="https://img1.tiancitycdn.com/project1/csol/event/2017/07/cs-map/170929/cs180131aa.jpg" /></a></li>'+
+'			<li><a href="javascript:;" ><img src="https://img1.tiancitycdn.com/project1/csol/event/2017/07/cs-map/170929/cs180131bb.jpg" /></a></li>'+
+'			<li><a href="javascript:;" ><img src="https://img1.tiancitycdn.com/project1/csol/event/2018/01/cs180103ver/images/180110.jpg" /></a></li>'+
+'			<li><a href="javascript:;" ><img src="https://img1.tiancitycdn.com/project1/csol/event/2017/07/cs-map/170929/51.jpg" /></a></li>'+
+'			<li><a href="javascript:;" ><img src="https://img1.tiancitycdn.com/project1/csol/event/2017/07/cs-map/170929/52.jpg" /></a></li>'+
+'			<li><a href="http://csolbbs.tiancity.com/forum.php?mod=viewthread&tid=3667&extra=page%3D1" target="_blank" ><img src="https://img1.tiancitycdn.com/project1/csol/event/2017/07/cs-map/170929/4.jpg" /></a></li>'+
+'			<li><a href="http://csolbbs.tiancity.com/forum.php?mod=viewthread&tid=3668&extra=page%3D1" target="_blank" ><img src="https://img1.tiancitycdn.com/project1/csol/event/2017/07/cs-map/170929/5.jpg" /></a></li>'+
+'			<li><a href="http://csolbbs.tiancity.com/forum.php?mod=viewthread&tid=3438&extra=page%3D1" target="_blank" ><img src="https://img1.tiancitycdn.com/project1/csol/event/2017/07/cs-map/171108/1.jpg" /></a></li>'+
+'			<li><a href="http://csolbbs.tiancity.com/forum.php?mod=viewthread&tid=3437&extra=page%3D1" target="_blank" ><img src="https://img1.tiancitycdn.com/project1/csol/event/2017/07/cs-map/171108/2.jpg" /></a></li>'+
+'			<li><a href="http://csolbbs.tiancity.com/forum.php?mod=viewthread&tid=3572&extra=page%3D1" target="_blank" ><img src="https://img1.tiancitycdn.com/project1/csol/event/2017/07/cs-map/170929/11.jpg" /></a></li>'+
+'			<li><a href="http://csolbbs.tiancity.com/forum.php?mod=viewthread&tid=3571&extra=page%3D1" ><img src="https://img1.tiancitycdn.com/project1/csol/event/2017/07/cs-map/170929/22.jpg" /></a></li>'+
+'			<li><a href="http://csolbbs.tiancity.com/forum.php?mod=viewthread&tid=2746&extra=page%3D2" target="_blank" ><img src="https://img1.tiancitycdn.com/project1/csol/event/2017/07/cs-map/170929/3.jpg" /></a></li>'+
+
+'			<li><a href="http://csolbbs.tiancity.com/forum.php?mod=viewthread&tid=1946" target="_blank" ><img src="https://img1.tiancitycdn.com/project1/csol/event/2017/07/cs-map/170904/1.jpg" /></a></li>'+
+'			<li><a href="http://csolbbs.tiancity.com/forum.php?mod=viewthread&tid=1948" target="_blank" ><img src="https://img1.tiancitycdn.com/project1/csol/event/2017/07/cs-map/170904/3.jpg" /></a></li>'+
+'			<li><a href="http://csolbbs.tiancity.com/forum.php?mod=viewthread&tid=1949" target="_blank" ><img src="https://img1.tiancitycdn.com/project1/csol/event/2017/07/cs-map/170904/4.jpg" /></a></li>'+
+'        </ul>'+
+'		</div>'+
+'			<a href="javascript:void(0);" class="goL" >左移一次</a> '+
+'			<a href="javascript:void(0);" class="goR">右移一次</a>'+
+'	</div>'+
+'</div>'
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
